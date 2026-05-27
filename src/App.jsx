248c1c717b2899d902c96cc1965d7de2ecbbe6e6
Siddharthan <siddharthansp07@gmail.com>
2026-05-27 18:36:27 +0530

    (commit5) added video playing function, fixed the position of the video, organised the image/video file locations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import tvImage from "./assets/crt_tv.png";
-import bgImage from "./assets/bg.png";
+import tvImage from "./assets/img/crt_tv.png";
+import bgImage from "./assets/img/bg.png";
+import myVideo from "./assets/videos/rr.mp4";
 
-export default function App() {
+export default function App() 
+{
   return (
-    <div style={{
+    <div style=
+    {{
       minHeight: "100vh",
+      width: "100vw",
       backgroundImage: `url(${bgImage})`,
       backgroundSize: "cover",
       backgroundPosition: "center",
@@ -12,7 +16,38 @@ export default function App() {
       alignItems: "center",
       justifyContent: "center",
     }}>
-      <img src={tvImage} alt="Old CRT TV" style={{ width: 560 }} />
+      <div style={{ position: "relative", width: 660 }}>
+
+        {/* Video sits behind the TV image */}
+        <video
+          src={myVideo}
+          autoPlay
+          loop
+          muted
+          style={{
+            position: "absolute",
+            top: "10%",
+            left: "9%",
+            width: "70%",
+            height: "80%",
+            objectFit: "cover",
+            zIndex: 1,
+          }}
+        />
+
+        {/* TV image on top */}
+        <img
+          src={tvImage}
+          alt="Old CRT TV"
+          style={{
+            position: "relative",
+            width: "100%",
+            zIndex: 2,
+            display: "block",
+          }}
+        />
+
+      </div>
     </div>
   );
 }
