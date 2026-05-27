b2099c7acd662300b775a41cb5598aee405e6825
Siddharthan <siddharthansp07@gmail.com>
2026-05-27 13:30:18 +0530

    (commit2) Added a tv image and added the code to display the img.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
+import tvImage from "./assets/crt_tv.png";
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div style={{
+      minHeight: "100vh",
+      background: "#111",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }}>
+      <img src={tvImage} alt="Old CRT TV" style={{ width: 560 }} />
     </div>
   );
 }
-
-export default App;
